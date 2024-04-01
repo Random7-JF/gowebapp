4287867a103a2e4ecf5a29cdaacd3b442723b00f
Jon Fingas <jon.fingas@gmail.com>
2024-03-31 20:13:16 -0500

    Added basic net/http server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("This will be a rebuild")
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("hit: /")
+		w.Write([]byte("Hello"))
+	})
+	router.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hit: /{name}")
+		id := r.PathValue("name")
+		w.Write([]byte(fmt.Sprintf("Hello, %s", id)))
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
 }
